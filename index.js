d55235fed4f18ca17436bad4ebe8a377ffc3f3f9
kolavardhan <kolavardhan95@gmail.com>
2026-01-06 14:16:58 +0530

    Added logic

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const toFahrenheit = document.getElementById("toFahrenheit")
-const toCelsius = document.getElementById("toCelsius")
-const convertBtn = document.getElementById("convertBtn")
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+const userInput = document.getElementById("userInput");
 
-function convert() {
+let temp;
+
+function convert(event) {
+    event.preventDefault(); 
+
+    const value = Number(userInput.value); 
+
     if (toFahrenheit.checked) {
-        
+        temp = (value * 9 / 5) + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    }
+    else if (toCelsius.checked) {
+        temp = (value - 32) * 5 / 9;
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else {
+        result.textContent = "Select a unit";
     }
 }
