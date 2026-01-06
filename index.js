eacf009d66c538fc4dcc3f1ecdc818d79c04e98d
kolavardhan <kolavardhan95@gmail.com>
2026-01-06 14:02:57 +0530

    Added initial JS code

---
+++ index.js
@@ -0,0 +1,9 @@
+const toFahrenheit = document.getElementById("toFahrenheit")
+const toCelsius = document.getElementById("toCelsius")
+const convertBtn = document.getElementById("convertBtn")
+
+function convert() {
+    if (toFahrenheit.checked) {
+        
+    }
+}
